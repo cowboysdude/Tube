3c92072563a6e79648b803f9048f18dc5db1597f
John Wade <johnw8017@gmail.com>
2018-12-28 21:54:02 -0500

    Update MMM-Tube.js

--- MMM-Tube.js
+++ MMM-Tube.js
@@ -1,85 +1,86 @@
 /* Magic Mirror
  * Module: MMM-Tube
  *
- * By cowboysdude
+ * Main.js by cowboysdude
  * 
  */
 var $, jQuery;
 
-Module.register("MMM-Tube", {
+Module.register('MMM-Tube', {
 
     // Module config defaults.
     defaults: {
         updateInterval: 60 * 1000, // every 10 minutes
         animationSpeed: 10,
         initialLoadDelay: 875, // 0 seconds delay
-        retryDelay: 1500,
-        channel: "",
-        playlist: "",
+        retryDelay: 1500, 
+        playlist: '',
         useChannel: false,
-        maxWidth: "400px",
-        fadeSpeed: 7 
+        maxWidth: '400px',
+        fadeSpeed: 7,
+        debug: false,
+        rotateInterval: 5 * 1000,
+        items: 5		
     },
 
     getStyles: function() {
-        return ["jquery.fancybox.min.css"];
-    },
-
-    WordWrap: function(str, width, brk, cut) {
-        brk = brk || 'n';
-        width = width || 75;
-        cut = cut || false;
-        if (!str) {
-            return str;
-        }
-        var regex = '.{1,' + width + '}(\s|$)' + (cut ? '|.{' + width + '}|.+$' : '|\S+?(\s|$)');
-        return str.match(RegExp(regex, 'g')).join(brk);
+        return ['jquery.fancybox.min.css'];
     },
 
     // Define start sequence.
     start: function() {
-        Log.info("Starting module: " + this.name);
-        this.sendSocketNotification("CONFIG", this.config);
+        Log.info('Starting module: ' + this.name);
+        this.sendSocketNotification('CONFIG', this.config);
 
         // Set locale. 
-        this.today = "";
+        this.today = '';
         this.tube = [];
         this.updateInterval = null;
         this.scheduleUpdate();
-		this.loaded = false;
+        this.loaded = false;
+        this.activeItem = 0;
+        this.rotateInterval = null;
     },
 
+    scheduleCarousel: function() {
+        console.log('Rotating next items');
+        this.rotateInterval = setInterval(() => {
+            this.activeItem++;
+            this.updateDom(this.config.animationSpeed);
+        }, this.config.rotateInterval);
+    },
 
     getDom: function() {
 
-        var wrapper = document.createElement("null");
+        var wrapper = document.createElement('null');
 
         //loading all external js files this way//		
 		
         var scriptElement = document.createElement('script');
         scriptElement.type = 'text/javascript';
-        scriptElement.src = "modules/MMM-Tube/jquery-3.3.1.min.js";
+        scriptElement.src = 'modules/MMM-Tube/jquery-3.3.1.min.js';
         document.body.appendChild(scriptElement);
 
         var scriptElement2 = document.createElement('script');
         scriptElement2.type = 'text/javascript';
-        scriptElement2.src = "modules/MMM-Tube/jquery.fancybox.js";
+        scriptElement2.src = 'modules/MMM-Tube/jquery.fancybox.js';
         document.body.appendChild(scriptElement2);
 
-		//Done loading external js files//
+        //Done loading external js files//
 
         var tube = this.tube;
-		 
-		
-		   var contain = document.createElement("div");
-		  contain.setAttribute("style", "margin-left:auto;","margin-right:auto;");
-		   wrapper.appendChild(contain);
+        console.log(tube);
         
-          tube.forEach(function(video, i) { 
-  
-  
+		   var keys = Object.keys(this.tube);
+ 
+        if (keys.length > 0) { 
+            if (this.activeItem >= keys.length) {
+                this.activeItem += 0;
+            }
+            var video = this.tube[keys[this.activeItem]];  
+			
             var titel = document.createElement(null);
-            titel.addEventListener("click", () => showvid(this));
+            titel.addEventListener('click', () => showvid(this));
             titel.innerHTML =
                 `<div class="item">
 				<div class="tooltip">
@@ -90,11 +91,21 @@ Module.register("MMM-Tube", {
              </div> 
 		     </div>`;
             wrapper.appendChild(titel);
+		 
+           
+		   $('.video-deck .card-body').on('click', function() {
+                $(this).parent().find('a').trigger('click');
+          
+		     });
 
-           $('.video-deck .card-body').on('click', function() {
-           $(this).parent().find("a").trigger('click');
-           });
-    });
+		  }
+        //borrowed from MMM-TouchNews modified to fit my needs//
+		   function showvid(thisvid) {
+            clearInterval(thisvid.rotateInterval);
+            $(document).on('afterClose.fb', () => thisvid.scheduleCarousel(this));
+        }
+         
+   
         return wrapper;
     }, 
 
@@ -112,12 +123,15 @@ Module.register("MMM-Tube", {
     },
 
     getTube: function() {
-        this.sendSocketNotification("GET_TUBE");
+        this.sendSocketNotification('GET_TUBE');
     },
 
     socketNotificationReceived: function(notification, payload) {
-        if (notification === "TUBE_RESULT") {
+        if (notification === 'TUBE_RESULT') {
             this.processTube(payload);
+        }
+		  if (this.rotateInterval == null) {
+            this.scheduleCarousel();
         }
         this.updateDom(this.config.initialLoadDelay);
     },
